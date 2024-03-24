5c1c2ef6770bc0decd0e561ebdc5f99ab1335653
VDFOREVER <vdforever@proton.me>
2024-03-24 15:28:34 +0300

    add gtk theme

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -16,8 +16,32 @@
     grimblast
     swappy
     wl-clipboard
-    gnome.adwaita-icon-theme
     xdg-utils
     xdg-desktop-portal-hyprland 
+    dconf
   ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;t
+      name = "Flat-Remix-GTK-Grey-Dark";
+    };
+
+    cursorTheme = {
+      package = pkgs.capitaine-cursors;
+      name = "capitaine-cursors-light";
+      size = 24;
+    };
+
+    iconTheme = {
+      package = pkgs.tela-icon-theme;
+      name = "Tela-black";
+    };
+
+    #iconTheme = {
+    #  package = pkgs.flat-remix-icon-theme;
+    #  name = "Flat-Remix-Black-Dark";
+    #};
+  };
 }
